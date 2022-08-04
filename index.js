a9d034a15af65b6f1aff56c0044c8f069d67b13e
董梦珂 <dongmk@dongmengke-ppadmindeMacBook-Pro.local>
2022-08-04 10:20:45 +0800

    demo

--- index.js
+++ index.js
@@ -1,30 +1,38 @@
-const express = require('express')
-const http = require('http')
-const { Server } = require('socket.io')
-const app = express()
-const server = http.createServer(app)
-const io = new Server(server)
+
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res) {
+   res.sendfile('index.html');
+});
+
+//Whenever someone connects this gets executed
+io.on('connection', function(socket) {
+   console.log('A user connected');
+
+   setTimeout(function() {
+    // socket.send('Sent a message 4seconds after connection!');
+    socket.emit('testerEvent', { descriptionA: 'A custom event named testerEvent!'});
+
+ }, 4000);
+
+   //Whenever someone disconnects this piece of code executed
+   socket.on('disconnect', function () {
+      console.log('A user disconnected');
+   });
+});
+
+var nsp = io.of('/my-namespace');
+nsp.on('connection', function(socket) {
+   console.log('someone connected');
+   setTimeout(function() {
+    // nsp.send('Hello everyone!');
+    nsp.emit('hi', { descriptionB: 'B custom event named testerEvent!'});
+   }, 4000)
+});
 
 
-app.get('/', (req, res) => {
-   res.sendFile(__dirname + '/index.html')
-})
-
-io.on('connect', (socket) => {
-    socket.on('chat message', msg => {
-        console.log('msg: ', msg)
-    })
-})
-
-io.on('connection', (socket) => {
-    socket.on('chat message', (msg) => {
-        io.emit('chat message', msg);
-      });
-  });
-
-io.emit('some event', { someProperty: 'some value', otherProperty: 'other value' })
-
-
-server.listen(3000, () => {
-    console.log('listening on *:3000')
-})
+http.listen(3000, function() {
+   console.log('listening on *:3000');
+});
